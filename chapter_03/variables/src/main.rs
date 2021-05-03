d42020fd84225a2d35a1aeae79babd3b4a9f4510
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2021-05-03 21:39:19 +0100

    Extra variables

--- chapter_03/variables/src/main.rs
+++ chapter_03/variables/src/main.rs
@@ -4,4 +4,41 @@ fn main() {
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+
+    // addition
+    let sum = 5 + 10;
+    println!("The value of sum is {}", sum);
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("The value of difference is {}", difference);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("The value of product is {}", product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("The value of quotient is {}", quotient);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("The value of remainder is {}", remainder);
+
+    // Extras
+    // division
+    let quotient = 7 / 2;
+    println!("The value of quotient is {}", quotient);
+    // remainder
+    let remainder = 7 % 2;
+    println!("The value of remainder is {}", remainder);
+
+    let f: bool = false;
+    println!("The value of f is {}", f);
+
+    let c = 'c';
+    println!("The value of c is {}", c);
+
+    let z = 'Z';
+    println!("The value of z is {}", z);
 }
